4bd0f8160e5121901549f6b1b63ffd3dbfeb28a6
mudassir ali <mudassirali2700@gmail.com>
2019-04-17 20:36:28 +0530

    changes made

---
+++ src/jUnitTestingPackage/jUnitFunction.java
@@ -0,0 +1,17 @@
+package jUnitTestingPackage;
+
+public class jUnitFunction {
+	
+	public int addNumbers(int num1,int num2) {
+		return num1+num2;
+	}
+
+	public String addStrings(String s1,String s2) {
+		return s1+s2;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
